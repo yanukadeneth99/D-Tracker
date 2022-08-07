f2d19a1757cb0827f92b10965fc67f6fb4f2245b
yanukadeneth99 <yanukadeneth99@gmail.com>
2022-08-08 00:23:26 +0530

    Squashed commit of the following:
    commit 76ab5b267e3fa837e35263e573a792321fc8f786
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Mon Aug 8 00:22:49 2022 +0530
    
        Delete unknown-31337.json
    
    commit 12bef57628ebed6bd49da1da43be87b1addb244a
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Mon Aug 8 00:16:43 2022 +0530
    
        Changed README
    
    commit f08fa3332059da61623ba9010d44a312bf970699
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Sun Jul 31 19:17:31 2022 +0530
    
        Finished the contract with comments
    
    commit bca5c4a6f5476d94348b895991bf722ba99b90f0
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Sun Jul 31 16:37:07 2022 +0530
    
        Made the delete section
    
    commit 115d674838803f594d2a143d2832551de088fb54
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Sat Jul 30 18:39:22 2022 +0530
    
        Worked on Creations
    
    commit 8d802dd58b37e1b968a434a01afa36a974911f11
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Sat Jul 30 04:15:49 2022 +0530
    
        Fix the Getters
    
    commit d951e7541e21aa9a38879d87d57527fe265da059
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Sat Jul 30 03:13:33 2022 +0530
    
        Made the Account struct

--- backend/test/ETracker.ts
+++ backend/test/ETracker.ts
@@ -2,6 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
 import { ContractFactory } from "ethers";
+import { ETracker } from "../typechain-types";
 
 describe("ETracker Tests", () => {
   async function deployOneYearLockFixture() {
@@ -10,12 +11,26 @@ describe("ETracker Tests", () => {
     const ETrackerV1 = (await ethers.getContractFactory(
       "ETracker"
     )) as ContractFactory;
-    const etrackerv1 = await upgrades.deployProxy(ETrackerV1, { kind: "uups" });
+    const etrackerv1 = (await upgrades.deployProxy(ETrackerV1, {
+      kind: "uups",
+    })) as ETracker;
 
     return { ETrackerV1, etrackerv1, owner, addr1 };
   }
 
   it("Runs", async function () {
     const { etrackerv1 } = await loadFixture(deployOneYearLockFixture);
+  });
+
+  it("CRUD Account", async () => {
+    const { etrackerv1, addr1 } = await loadFixture(deployOneYearLockFixture);
+    await etrackerv1
+      .connect(addr1)
+      .createAccount(ethers.utils.formatBytes32String("Henry"));
+    expect(await etrackerv1.connect(addr1).getName()).to.equal("Henry");
+    await etrackerv1.connect(addr1).updateName("James");
+    expect(await etrackerv1.connect(addr1).getName()).to.equal("James");
+    await etrackerv1.connect(addr1).deleteAccount();
+    expect(await etrackerv1.connect(addr1).getAccount()).to.be.false;
   });
 });
